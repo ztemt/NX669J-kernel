066e863fbd30924ca61d963882f500ee38dacbe2
nubia_public <li.haibin@nubia.com>
2021-09-02 10:54:32 +0800

    second: add sound that ignored first time

---
+++ msm-5.4/scripts/kconfig/.conf.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/kconfig/conf := /home/nubia/shenzx/NX669J/ROM/Production/app/prebuilts/clang/host/linux-x86/clang-r383902b/bin/clang   -o scripts/kconfig/conf scripts/kconfig/conf.o scripts/kconfig/confdata.o scripts/kconfig/expr.o scripts/kconfig/lexer.lex.o scripts/kconfig/parser.tab.o scripts/kconfig/preprocess.o scripts/kconfig/symbol.o   
